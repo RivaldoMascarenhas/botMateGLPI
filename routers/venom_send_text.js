e914c36b8da5144067687a18d6c2540066a91068
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2025-09-01 22:02:26 -0300

    feat: add phone number handling in text processing and GLPI ticket creation

--- routers/venom_send_text.js
+++ routers/venom_send_text.js
@@ -9,26 +9,29 @@ const router = express.Router();
 
 router.post("/venom/text", async (req, res) => {
   try {
-    const { user, text } = textRequestSchema.parse(req.body);
+    const { user, phone, text } = textRequestSchema.parse(req.body);
     logger.info(`Recebida mensagem vinda do bot. ${user}: ${text}`);
     //lógica que envia para a ia
     const response = await sendTextToIA(text);
     logger.info(`Resposta da IA para ${user}: ${response}`);
+    //lógica que transforma a resposta da ia em um ticket
     const newTicket = JSON.parse(response);
-    newTicket = { ...newTicket, requester_name: user }; //adiciona o nome do usuário que enviou a mensagem
+    newTicket = { ...newTicket, requester_name: user }; // Adiciona o nome do requerente e o telefone ao ticket
 
     //lógica que envia para o glpi
     const glpiResponse = await glpiCreateTicket(newTicket);
+
+    glpiResponse = { ...glpiResponse, phone }; // Adiciona o telefone na resposta do GLPI
     logger.info(`Resposta do GLPI para ${user}: ${glpiResponse}`);
 
-    res.json({ response });
+    res.json({ ticket: glpiResponse });
   } catch (error) {
     if (error instanceof z.ZodError) {
       logger.warn(`Validação falhou: ${JSON.stringify(error.errors)}`);
       return res.status(400).json({ errors: error.errors });
     }
-    logger.error(`Erro ao enviar texto para IA: ${error.message}`);
-    res.status(500).json({ error: "Error sending text to IA" });
+    logger.error(`Erro ao processar solicitação: ${error.message}`);
+    res.status(500).json({ error: "Erro interno do servidor" });
   }
 });
 
