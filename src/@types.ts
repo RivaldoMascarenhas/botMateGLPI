a5681a567fe7e37459234a8c2306c06d4c428f28
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2025-09-04 12:06:23 -0300

    feat:Adds new API response type definitions
    Introduces types for initializing a session and creating a ticket, enabling new functionalities. Extends the `ResponseIA` interface with `requesttypes_id` and `userResquest` to enrich AI response data.

--- src/@types.ts
+++ src/@types.ts
@@ -1,6 +1,7 @@
 export interface ResponseIA {
   title: string;
   description: string;
+  requesttypes_id: number;
   urgencyText:
     | "Muito Baixa"
     | "Baixa"
@@ -8,5 +9,17 @@ export interface ResponseIA {
     | "Alta"
     | "Muito Alta"
     | "Crítica";
+  userResquest?: string;
   error?: string;
+}
+export interface initSessionResponse {
+  session_token: string;
+}
+export interface createTicketResponse {
+  id: number;
+  name: string;
+  content: string;
+  urgency: number;
+  status: number;
+  users_id_recipient: number;
 }
