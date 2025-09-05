dcbaad1a9c403bb6961c37ea5bc9daa3922bbe76
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2025-09-05 11:36:47 -0300

    feat:
    Refactors Venom router import for clarity
    
    Renames the imported router variable from `router` to `venomRouter` and updates its usage for improved code readability and consistency. Adjusts the import path to use the `.js` extension, aligning with module resolution practices.

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import pino from "pino";
 import pinoPretty from "pino-pretty";
 import cors from "cors";
 import express from "express";
-import router from "./routers/venom_send_text.ts";
+import venomRouter from "./routers/venom_send_text.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json({ limit: "2mb" }));
 app.use(cors());
-app.use("/", router);
+app.use("/", venomRouter);
 export const logger = pino(pinoPretty({ translateTime: "SYS:standard" }));
 
 if (
