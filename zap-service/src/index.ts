333a32f6c153d9ec07ab28628a8a014edb26935f
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2025-09-10 21:45:53 -0300

    feat: Refines ticket creation and contact handling
    Improves the robustness of contact information retrieval and name extraction by using `client.getContactById` and introducing a name validation utility.
    
    Adjusts the ticket creation flow to provide an informational message when a user sends only "chamado", and only proceeds with ticket submission for longer, more descriptive messages.
    
    Updates the backend endpoint for ticket submission and enhances error handling for both API calls and the initial client setup, providing more informative feedback.

--- zap-service/src/index.ts
+++ zap-service/src/index.ts
@@ -4,13 +4,12 @@ import express from "express";
 import dotenv from "dotenv";
 import axios from "axios";
 import { ResponseIA } from "./@type";
-import { message } from "./message";
+import { infoCreateTicket, message } from "./message";
+import { isValidName } from "./utils";
 
 dotenv.config();
 const PORT = process.env.PORT;
-
 const app = express();
-
 app.use(express.json());
 
 // Inicia o cliente WhatsApp com persistência
@@ -37,19 +36,36 @@ client.on("ready", () => {
 client.on("message", async (msg) => {
   const text = msg.body.toLowerCase();
   const sender = msg.from;
-  const contact = await msg.getContact();
+  let contact;
+  try {
+    contact = await client.getContactById(sender);
+  } catch (error) {
+    console.error(error);
+  }
 
-  if (text.includes("chamado")) {
-    console.log(
-      `📩 Mensagem recebida de ${contact.pushname.split(" ")[0]}: ${msg.body}`
-    );
+  // garante um nome válido
+  let name =
+    contact?.pushname?.split(" ")[0] ||
+    contact?.name?.split(" ")[0] ||
+    `${sender.replace("@c.us", "")}`;
+
+  if (!isValidName(name)) {
+    name = sender.replace("@c.us", "").replace("55", "");
+  }
+
+  if (text.toLowerCase().includes("chamado") && text.length == 7) {
+    await client.sendMessage(sender, infoCreateTicket(name));
+  }
+
+  if (text.toLowerCase().includes("chamado") && text.length > 7) {
+    console.log(`📩 Mensagem recebida de ${name}: ${msg.body}`);
     console.log(`📩 Enviando mensagem para o backend...`);
 
     try {
       const response = await axios.post<ResponseIA>(
-        "http://localhost:5001/ticket",
+        "http://localhost:4000/glpi/ticket",
         {
-          user: contact.pushname.split(" ")[0] || "Sem nome",
+          user: name,
           phone: Number(sender.replace("@c.us", "")),
           text: msg.body,
         }
@@ -61,15 +77,26 @@ client.on("message", async (msg) => {
       const messageSucess = message(response.data);
       await client.sendMessage(sender, messageSucess);
     } catch (error: any) {
-      console.error(error.response.data.error);
-      const message = `❌ Erro ao criar chamado: ${error.response.data.error}`;
+      console.error(error.response?.data?.error || error.message);
+      const message = `❌ Erro ao criar chamado: ${
+        error.response?.data?.error || "Erro desconhecido"
+      }`;
       await client.sendMessage(sender, message);
     }
   }
 });
 
+// Inicia o cliente
+async function start() {
+  try {
+    await client.initialize();
+  } catch (error) {
+    console.error("Erro ao iniciar o cliente:", error);
+  }
+}
+
 // Inicializa cliente
-client.initialize();
+start();
 
 // Endpoint para enviar mensagem manual
 app.post("/send", async (req, res) => {
